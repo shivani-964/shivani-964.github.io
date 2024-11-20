bb74baad3ad3db805e42a46f5f09d876e248f0ec
shivani-964 <shivanipatelthota@gmail.com>
2024-11-20 17:49:21 +0530

    modified

---
+++ temp.js
@@ -0,0 +1,16 @@
+db.products.insertMany(
+    [
+    {
+        "name": "Product 5",
+        "desc": "This is the description of product 5",
+        "price": 25,
+        "url": "https://picsum.photos/id/1410/300/300"
+    },
+    {
+        "name":"Product 6",
+        "desc":"This is the description of product 6",
+        "price":32,
+        "url":"https://picsum.photos/id/235/300/300"
+    }
+    ]
+);
